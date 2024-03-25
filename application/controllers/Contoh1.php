79ef600d514563a690cf965e20850e48438b1c5c
Ygloryy <gloryxzv@gmail.com>
2024-03-25 16:27:14 +0700

    Tugas1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+    class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya <i>Yolanda Gloria</i>
+         <br>Saya tinggal di <b>Pontianak</b>
+         <br>saya suka menonton film 
+         bergenre <b><i>Thriller</i></b>";
+ }
+}
