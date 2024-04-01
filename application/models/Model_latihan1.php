a0281ba38f1605bc1d4428804fa8d420bb74cd35
Ygloryy <gloryxzv@gmail.com>
2024-04-01 19:18:47 +0700

    Pertemuan 3

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -1,10 +1,10 @@
 <?php
     class Model_latihan1 extends CI_Model
 {
- //membuat variable untuk menampung nilai
+
     public $nilai1, $nilai2, $hasil;
 
- //method penjumlahan
+
     public function jumlah($n1 = null, $n2 = null)
  {
         $this->nilai1 = $n1;
